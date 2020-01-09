6e96100b0deffd93827850162af693dacf900aea
Git2020-CS <tjubjy2015@163.com>
2020-01-09 10:47:12 +0800

    Add all files

---
+++ perf/hot.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define N 1000000
+
+void calc()
+{
+    int i=0;
+    for(i=0;i<100;i++)
+    {
+
+    }
+}
+
+int main()
+{
+    int i;
+    for(i=0;i<N;i++)
+    {
+        calc();
+    }
+    return 0;
+}
+
